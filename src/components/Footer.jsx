3be7a0b17daae2f973022d221c4bfc821353b3d7
enochtagoe <enochtagoe@pursuit.org>
2024-02-02 11:49:19 -0500

    change email

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <section>
       <footer className="bg-warning text-center text-lg-end text-black">
-  <h5>Contact Us : skillshop@aol.com</h5>
+  <h5>Contact Us : skillshop@email.com</h5>
 </footer>
 
     </section>
