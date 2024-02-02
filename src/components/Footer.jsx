ef7190d92fc3cd691a0f28943b65876b3a1b11dd
enochtagoe <enochtagoe@pursuit.org>
2024-02-02 12:00:41 -0500

    fix footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,13 @@
-
+import "./Footer.css";
 
 const Footer = () => {
   return (
     <section>
       <footer className="bg-warning text-center text-lg-end text-black">
-  <h5>Contact Us : skillshop@email.com</h5>
-</footer>
-
+        <h5>Contact Us : <a href="mailto:skillshop@email.com" style={{ color: 'black' }}>skillshop@email.com</a></h5>
+      </footer>
     </section>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
