07225410c28e34cb00bba5a23571aa435e83172d
enochtagoe <enochtagoe@pursuit.org>
2024-02-02 13:04:43 -0500

    created new css

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import "../App.css"
+import "../components/Landing.css";
     /* Landing.jsx will have functionality (Link to navigate to All job posts and log in aside) while App.jsx will have the ternary
      function for location path which determines which layout to choose depending on path location (whether on Landing page view or other views). MainLayout.jsx serves to wrap all the components that will not have landing page layout */
 
