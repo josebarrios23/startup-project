ff8e48732c72bcafada018ad3c74861859e6ace6
juligarc91 <julissagarcia@pursuit.org>
2024-02-04 19:51:14 -0500

    created new components

--- src/components/UserProjectCards.jsx
+++ src/components/UserProjectCards.jsx
@@ -1,1 +1,3 @@
 
+// create button (view will look like ProjectCards.jsx but only selected user project cards)
+// create button needs Link to route with component with form in it (CreateProjectForm.jsx)
---
+++ src/components/UserSingleProject.jsx
@@ -0,0 +1,3 @@
+// delete / edit post button
+// edit button takes you to EditPostForm.jsx
+// delete connects to api to delete the post
